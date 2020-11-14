041ceeb15b777e1f83dc406ddf1e130c28d810bd
WELL ALKLINE KNOWLEDGE <68064582+mrspyder007@users.noreply.github.com>
2020-11-14 07:31:15 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,38 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import  FBscreen from './screens/fb';
+import  INscreen from './screens/in';
+
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+
+
+export default class App extends React.Component {
+
+
+  render(){
+
+  return (
+    <Appcontainer/>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+const TabContainer = createBottomTabNavigator({
+ FB:{screen:FBscreen},
+ IN:{screen:INscreen}
+})
+const Appcontainer = createAppContainer(TabContainer)
+
